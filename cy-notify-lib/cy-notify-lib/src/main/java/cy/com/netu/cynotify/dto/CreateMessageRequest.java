40f88b33aba4e401401337fbc6c37bed11958552
Christoforos Lambrou <christoforosll@netu.com.cy>
2025-03-24 10:19:14 +0000

    Update: 1.5

--- cy-notify-lib/cy-notify-lib/src/main/java/cy/com/netu/cynotify/dto/CreateMessageRequest.java
+++ cy-notify-lib/cy-notify-lib/src/main/java/cy/com/netu/cynotify/dto/CreateMessageRequest.java
@@ -43,6 +43,7 @@ public class CreateMessageRequest {
     @Getter @Setter
     private EnumNotificationChannel messageChannel;
 
+    @SuppressWarnings("checkstyle:ParameterNumber")
     @JsonCreator
     public CreateMessageRequest(
             @JsonProperty("relatedRecordType") String relatedRecordType,
